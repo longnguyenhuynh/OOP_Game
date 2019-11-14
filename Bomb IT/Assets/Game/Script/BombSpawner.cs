c17487fef6a52e27266588557ca937d9ee6f8371
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2019-11-14 22:57:34 +0700

    xong bomb

--- Bomb IT/Assets/Game/Script/BombSpawner.cs
+++ Bomb IT/Assets/Game/Script/BombSpawner.cs
@@ -4,16 +4,28 @@ public class BombSpawner : MonoBehaviour
 {
     public Tilemap tilemap;
     public GameObject bombPrefab;
+    public GameObject player = null;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int cell = tilemap.WorldToCell(worldPos);
-            Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    Vector3Int cell = tilemap.WorldToCell(worldPos);
+        //    Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
 
-            Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
-        }
+        //    Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if (player == null)
+        //        player = GameObject.Find("Player");
+
+        //    Vector3 worldPos = Camera.main.ScreenToWorldPoint(player.transform.position);
+        //    Vector3Int cell = tilemap.WorldToCell(worldPos);
+        //    Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+
+        //    Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+        //}
     }
 }
